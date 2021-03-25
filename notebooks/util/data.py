46b40efaee2b7b3c5bf7d42d372c6d20f27190d8
Luca Giuliani <luca.giuliani10@studio.unibo.it>
2021-03-26 00:52:52 +0100

    Refactor intervention file and Calibration notebook

--- notebooks/util/data.py
+++ notebooks/util/data.py
@@ -8,7 +8,7 @@ def get_regional_data(scaling_factor: float = 1, region: str = 'Emilia-Romagna')
     # extract data of a single region
     df = df[df['denominazione_regione'] == region]
     # reindex using date
-    df.index = [pd.to_datetime(d).date() for d in df['data']]
+    df.index = pd.Series([pd.to_datetime(d).date() for d in df['data']], name='date')
     # keep just some columns and rename them
     df = df[['ricoverati_con_sintomi', 'terapia_intensiva', 'totale_casi', 'deceduti', 'tamponi']]
     df.columns = ['n_severe', 'n_critical', 'cum_diagnoses', 'cum_deaths', 'new_tests']
@@ -16,7 +16,5 @@ def get_regional_data(scaling_factor: float = 1, region: str = 'Emilia-Romagna')
     df['new_tests'] = [t2 - t1 for t1, t2 in zip([0] + list(df['new_tests'][:-1]), df['new_tests'])]
     # rescaling according to given factor
     df = (df / scaling_factor).clip(lower=0.0)
-    # add date column and reset index
-    df['date'] = df.index
-    df.index = range(len(df))
-    return df
+    # reset index to get date as a column
+    return df.reset_index()
--- notebooks/util/interventions.py
+++ notebooks/util/interventions.py
@@ -1,121 +1,152 @@
 import pandas as pd
 from typing import List, Dict
 import covasim as cv
-from numba.tests.test_parallel_backend import mask_runner
 
 
-def get_delta(date: str, start_date: str = '2020-02-24') -> int:
-    return (pd.to_datetime(date).date() - pd.to_datetime(start_date).date()).days
+class Interventions:
+    def __init__(self, start_date='2020-02-24', test_column='new_tests', **kwargs: Dict[str, float]):
+        self.start_date = start_date
+        self.test_column = test_column
+        self.parameters = kwargs
 
+    def get_delta(self, date: str) -> int:
+        return (pd.to_datetime(date).date() - pd.to_datetime(self.start_date).date()).days
 
-def get_vals(default: float = None, white: float = None, yellow: float = None, orange: float = None, red: float = None):
-    periods = pd.Series({
-        '2020-03-08': 'red',
-        '2020-05-18': 'white',
-        '2020-11-08': 'yellow',
-        '2020-11-15': 'orange',
-        '2020-12-10': 'yellow',
-        '2020-12-21': 'orange',
-        '2021-02-01': 'yellow',
-        '2021-02-21': 'orange',
-        '2021-03-01': 'red'
-    })
-    periods.index = [get_delta(d) for d in periods.index]
-    return periods if default is None else periods.map({
-        'white': default if white is None else white,
-        'yellow': default if yellow is None else yellow,
-        'orange': default if orange is None else orange,
-        'red': default if red is None else red
-    })
+    def get_values(self, postfix: str = '', **kwargs: Dict[str, float]) -> pd.Series:
+        periods = pd.Series({
+            self.start_date: 'init',
+            '2020-03-08': 'red',
+            '2020-05-18': 'summer',
+            '2020-11-08': 'yellow',
+            '2020-11-15': 'orange',
+            '2020-12-10': 'yellow',
+            '2020-12-21': 'orange',
+            '2021-02-01': 'yellow',
+            '2021-02-21': 'orange',
+            '2021-03-01': 'red'
+        }, name='periods')
+        periods.index = [self.get_delta(d) for d in periods.index]
+        return periods.map({k: kwargs.get(f'{k}{postfix}') for k in periods.unique()})
 
+    # SENSITIVITY: https://www.health.harvard.edu/blog/which-test-is-best-for-covid-19-2020081020734
+    def tests(self) -> cv.Intervention:
+        return cv.test_num(self.test_column, quar_policy='both', sensitivity=0.8, do_plot=False)
 
-# SENSITIVITY: https://www.health.harvard.edu/blog/which-test-is-best-for-covid-19-2020081020734
-def tests() -> cv.Intervention:
-    return cv.test_num('new_tests', quar_policy='both', sensitivity=0.8, do_plot=False)
+    def contact_tracing(self) -> cv.Intervention:
+        defaults = dict(
+            household_trace_prob=1.0,
+            household_trace_time=0.0,
+            school_trace_prob=self.parameters.get('trace_prob', 0.0),
+            school_trace_time=self.parameters.get('trace_time', 0.0),
+            work_trace_prob=self.parameters.get('trace_prob', 0.0),
+            work_trace_time=self.parameters.get('trace_time', 0.0),
+            casual_trace_prob=0.0,
+            casual_trace_time=0.0
+        )
+        defaults.update(self.parameters)
+        return cv.contact_tracing(
+            trace_probs=dict(
+                h=defaults['household_trace_prob'],
+                s=defaults['school_trace_prob'],
+                w=defaults['work_trace_prob'],
+                c=defaults['casual_trace_prob']
+            ),
+            trace_time=dict(
+                h=defaults['household_trace_time'],
+                s=defaults['school_trace_time'],
+                w=defaults['work_trace_time'],
+                c=defaults['casual_trace_time']
+            ),
+            do_plot=False
+        )
 
+    def smart_working(self) -> cv.Intervention:
+        defaults = dict(
+            init_work_contacts=1.,
+            summer_work_contacts=1.,
+            yellow_work_contacts=1.,
+            orange_work_contacts=1.,
+            red_work_contacts=self.parameters.get('work_contacts')
+        )
+        defaults.update(self.parameters)
+        assert defaults['red_work_contacts'] is not None, 'red_work_contacts is required'
+        v = self.get_values(postfix='_work_contacts', **defaults)
+        return cv.clip_edges(days=v.index.values, changes=v.values, layers='w')
 
-def contact_tracing(trace_prob: float, trace_time: float) -> cv.Intervention:
-    return cv.contact_tracing(
-        trace_probs=dict(h=1.0, s=trace_prob, w=trace_prob, c=0.0),
-        trace_time=dict(h=0, s=trace_time, w=trace_time),
-        do_plot=False
-    )
+    def schools_closed(self) -> cv.Intervention:
+        defaults = dict(
+            init_school_contacts=1.,
+            summer_school_contacts=0.,
+            yellow_school_contacts=self.parameters.get('school_contacts'),
+            orange_school_contacts=self.parameters.get('school_contacts'),
+            red_school_contacts=0.
+        )
+        defaults.update(self.parameters)
+        assert defaults['yellow_school_contacts'] is not None, 'yellow_school_contacts is required'
+        assert defaults['orange_school_contacts'] is not None, 'orange_school_contacts is required'
+        v = self.get_values(postfix='_school_contacts', **defaults)
+        return cv.clip_edges(days=v.index.values, changes=v.values, layers='s')
 
+    def lockdown_interactions(self) -> cv.Intervention:
+        defaults = dict(
+            init_casual_contacts=1.,
+            summer_casual_contacts=1.,
+            yellow_casual_contacts=1.,
+            orange_casual_contacts=self.parameters.get('casual_contacts'),
+            red_casual_contacts=0.
+        )
+        defaults.update(self.parameters)
+        assert defaults['orange_casual_contacts'] is not None, 'orange_casual_contacts is required'
+        v = self.get_values(postfix='_casual_contacts', **defaults)
+        return cv.clip_edges(days=v.index.values, changes=v.values, layers='c')
 
-# smart working from 08/03 to 18/05
-def smart_working(work_contacts: float) -> cv.Intervention:
-    v = get_vals(default=1., red=work_contacts)
-    return cv.clip_edges(days=v.index.values, changes=v.values, layers='w')
+    # regional lockdowns to avoid imported cases
+    def imported_cases(self) -> cv.Intervention:
+        defaults = dict(
+            init_imports=0.,
+            summer_imports=self.parameters.get('init_imports', 0.),
+            yellow_imports=self.parameters.get('init_imports', 0.),
+            orange_imports=self.parameters.get('init_imports', 0.),
+            red_imports=self.parameters.get('init_imports', 0.)
+        )
+        defaults.update(self.parameters)
+        v = self.get_values(postfix='_imports', **defaults)
+        return cv.dynamic_pars(n_imports=dict(days=v.index.values, vals=v.values))
 
+    # summer viral load reduction
+    def viral_load_reduction(self) -> cv.Intervention:
+        assert 'init_beta' in self.parameters
+        days = [0, self.get_delta('2020-05-15'), self.get_delta('2020-09-15')]
+        defaults = dict(
+            init_beta=self.parameters['init_beta'],
+            init_symp=self.parameters.get('init_symp', 1.),
+            init_sev=self.parameters.get('init_sev', 1.),
+            init_crit=self.parameters.get('init_crit', 1.),
+            init_death=self.parameters.get('init_death', 1.)
+        )
+        args = {
+            **defaults,
+            **{key.replace('init', 'summer'): val for key, val in defaults.items()},
+            **{key.replace('init', 'winter'): val for key, val in defaults.items()}
+        }
+        args.update(self.parameters)
+        return cv.dynamic_pars(dict(
+            beta=dict(days=days, vals=[args['init_beta'], args['summer_beta'], args['winter_beta']]),
+            rel_symp_prob=dict(days=days, vals=[args['init_symp'], args['summer_symp'], args['winter_symp']]),
+            rel_severe_prob=dict(days=days, vals=[args['init_sev'], args['summer_sev'], args['winter_sev']]),
+            rel_crit_prob=dict(days=days, vals=[args['init_crit'], args['summer_crit'], args['winter_crit']]),
+            rel_death_prob=dict(days=days, vals=[args['init_death'], args['summer_death'], args['winter_death']])
+        ))
 
-# schools closed from 08/03 to 15/09, then blended modality until orange zone
-def schools_closed(school_contacts: float) -> cv.Intervention:
-    v = get_vals(default=0., yellow=school_contacts, orange=school_contacts)
-    return cv.clip_edges(days=v.index.values, changes=v.values, layers='s')
-
-
-# lockdown from 08/03, then no lockdown, then orange/yellow zones
-def lockdown_interactions(yellow_contacts: float, orange_contacts: float) -> cv.Intervention:
-    v = get_vals(default=1., yellow=yellow_contacts, orange=orange_contacts, red=0.)
-    return cv.clip_edges(days=v.index.values, changes=v.values, layers='c')
-
-
-def masks(winter_change: float) -> cv.Intervention:
-    return cv.change_beta(days=[get_delta('2020-09-15')], changes=[winter_change], layers=['w', 's', 'c'])
-
-
-# regional lockdowns to avoid imported cases
-def imported_cases(white_imp: float, yellow_imp: float, orange_imp: float) -> cv.Intervention:
-    v = get_vals(default=0., white=white_imp, yellow=yellow_imp, orange=orange_imp)
-    return cv.dynamic_pars(n_imports=dict(days=v.index.values, vals=v.values))
-
-
-# summer viral load reduction
-def viral_load_reduction(
-        summer_beta: float, summer_symp: float, summer_sev: float, summer_crit: float, summer_death: float,
-        winter_beta: float, winter_symp: float, winter_sev: float, winter_crit: float, winter_death: float
-) -> cv.Intervention:
-    days = [get_delta('2020-05-15'), get_delta('2020-09-15')]
-    dynamic_pars = {}
-    if summer_beta is not None and winter_beta is not None:
-        dynamic_pars['beta'] = dict(days=days, vals=[summer_beta, winter_beta])
-    if summer_symp is not None and winter_symp is not None:
-        dynamic_pars['rel_symp_prob'] = dict(days=days, vals=[summer_symp, winter_symp])
-    if summer_sev is not None and winter_sev is not None:
-        dynamic_pars['rel_severe_prob'] = dict(days=days, vals=[summer_sev, winter_sev])
-    if summer_crit is not None and winter_crit is not None:
-        dynamic_pars['rel_crit_prob'] = dict(days=days, vals=[summer_crit, winter_crit])
-    if summer_death is not None and winter_death is not None:
-        dynamic_pars['rel_death_prob'] = dict(days=days, vals=[summer_death, winter_death])
-    return None if len(dynamic_pars) == 0 else cv.dynamic_pars(dynamic_pars)
-
-
-def get_interventions(p: Dict[str, float]) -> List[cv.Intervention]:
-    interventions = [tests()]
-
-    if p.get('trace_prob') is not None and p.get('trace_time') is not None:
-        interventions.append(contact_tracing(p['trace_prob'], p['trace_time']))
-
-    if p.get('work_contacts') is not None:
-        interventions.append(smart_working(p['work_contacts']))
-
-    if p.get('school_contacts') is not None:
-        interventions.append(schools_closed(p['school_contacts']))
-
-    if p.get('yellow_contacts') is not None and p.get('orange_contacts') is not None:
-        interventions.append(lockdown_interactions(p['yellow_contacts'], p['orange_contacts']))
-
-    if p.get('winter_beta_change') is not None:
-        interventions.append(masks(p['winter_beta_change']))
-
-    if p.get('white_imp') is not None and p.get('yellow_imp') is not None and p.get('orange_imp') is not None:
-        interventions.append(imported_cases(p['white_imp'], p['yellow_imp'], p['orange_imp']))
-
-    vlr = viral_load_reduction(
-        p.get('summer_beta'), p.get('summer_symp'), p.get('summer_sev'), p.get('summer_crit'), p.get('summer_death'),
-        p.get('winter_beta'), p.get('winter_symp'), p.get('winter_sev'), p.get('winter_crit'), p.get('winter_death')
-    )
-    if vlr is not None:
-        interventions.append(vlr)
-
-    return interventions
+    def get_all(self, exclude=None) -> List[cv.Intervention]:
+        exclude = [] if exclude is None else exclude
+        interventions = [
+            self.tests,
+            self.contact_tracing,
+            self.smart_working,
+            self.schools_closed,
+            self.lockdown_interactions,
+            self.imported_cases,
+            self.viral_load_reduction
+        ]
+        return [inter() for inter in interventions if inter.__name__ not in exclude]
